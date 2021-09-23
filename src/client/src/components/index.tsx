cae601e9b29c7ad97c6189cfab807a9df791f45b
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-09-23 15:02:09 +0100

    heroku buildpack fix

--- src/client/src/components/index.tsx
+++
@@ -1,3 +0,0 @@
-export {default as Navbar} from './nav/Navbar';
-export {default as ProfileCard} from './result/ProfileCard';
-export {default as BarChart} from './result/BarChart';
--- src/client/src/pages/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as Home }            from './Home';
-export { default as About }           from './About';
-export { default as Form }            from './Form';
-export { default as Result }          from './Result'
-export { default as PageNotFound }    from './PageNotFound';
